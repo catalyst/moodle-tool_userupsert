4525d276b120513587d239d3446f4935526b4f4d
Misha Golenkov <mikhailgolenkov@catalyst-au.net>
2024-05-14 10:09:27 +1000

    Bump CI up up Moodle 4.4

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ $plugin->component = 'tool_userupsert';
 $plugin->version = 2023010500;
 $plugin->release = '2023010500';
 $plugin->requires = 2020061500;
-$plugin->supported = [39, 401];  // Available as of Moodle 3.9.0 or later.
+$plugin->supported = [39, 404];  // Available as of Moodle 3.9.0 or later.
 $plugin->maturity = MATURITY_STABLE;
